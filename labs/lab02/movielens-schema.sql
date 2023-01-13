828ad82730a81db428ed9b9b8a76acb9e27d6968
Sarthak Mittal <sarthakmittal0902@gmail.com>
2023-01-13 17:04:36 +0530

    Q1, Q2 and Q6 done

--- labs/lab02/movielens-schema.sql
+++ labs/lab02/movielens-schema.sql
@@ -1,24 +1,24 @@
 create table links
-	(movieId		varchar(7),
+	(movieId		int,
 	 imdbId		varchar(7),
 	 tmdbId		varchar(7)
 	);
 create table movies
-	(movieId		varchar(7),
+	(movieId		int,
 	 title		varchar(200),
 	 genres		varchar(200)
 	);	
 	
 create table ratings
 	(userId		varchar(7),
-	 movieId		varchar(25),
+	 movieId		int,
 	 rating 	numeric(2,1),
 	 timestamp		int
 	);
 		
 create table tags
 	(userId		varchar(7),
-	 movieId		varchar(25),
+	 movieId		int,
 	 tag 	varchar(100),
 	 timestamp		int
 	);
