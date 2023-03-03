b7c6d633895d4c0c67184dd181a6662552a8ccb6
Sarthak Mittal <sarthakmittal0902@gmail.com>
2023-03-03 17:15:45 +0530

    Lab 6 ongoing

--- labs/lab06/scripts/compile.sh
+++ labs/lab06/scripts/compile.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-cd ../postgresql
+cd ..
+cd postgresql
+rm -rf $(pwd)/install/data
 export POSTGRES_INSTALLDIR=$(pwd)/install
 export POSTGRES_SRCDIR=$(pwd)
+cd ${POSTGRES_SRCDIR}
 export enable_debug=yes
+make | tee gmake.out # takes time
+make install | tee gmake_install.out
 export LD_LIBRARY_PATH=${POSTGRES_INSTALLDIR}/lib:${LD_LIBRARY_PATH}
 export PATH=${POSTGRES_INSTALLDIR}/bin:${PATH}
 export PGDATA=${POSTGRES_INSTALLDIR}/data
-${POSTGRES_INSTALLDIR}/bin/pg_ctl stop
-${POSTGRES_INSTALLDIR}/bin/pg_ctl -D $PGDATA -l logfile start # needed after stop
-${POSTGRES_INSTALLDIR}/bin/createdb -p 9432 $1
-${POSTGRES_INSTALLDIR}/bin/psql -p 9432 $1 # \q to quit
-${POSTGRES_INSTALLDIR}/bin/pg_ctl stop
+${POSTGRES_INSTALLDIR}/bin/initdb -D ${PGDATA}
+sed -i 's/#port = 5432/port = 9432/g' ${POSTGRES_INSTALLDIR}/data/postgresql.conf
 cd ../scripts
