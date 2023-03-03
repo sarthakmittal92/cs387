10c3ca1eeb640adcb39d630e8169ad765572f69b
Sarthak Mittal <sarthakmittal0902@gmail.com>
2023-03-03 13:39:41 +0530

    Adding drop script

--- labs/lab06/scripts/drop.sh
+++ labs/lab06/scripts/drop.sh
@@ -8,7 +8,6 @@ export LD_LIBRARY_PATH=${POSTGRES_INSTALLDIR}/lib:${LD_LIBRARY_PATH}
 export PATH=${POSTGRES_INSTALLDIR}/bin:${PATH}
 export PGDATA=${POSTGRES_INSTALLDIR}/data
 ${POSTGRES_INSTALLDIR}/bin/pg_ctl -D $PGDATA -l logfile start # needed after stop
-${POSTGRES_INSTALLDIR}/bin/createdb -p 9432 $1
+${POSTGRES_INSTALLDIR}/bin/dropdb -p 9432 $1
 ${POSTGRES_INSTALLDIR}/bin/pg_ctl stop
-postgres --single -D ${PGDATA} $1 # Ctrl-D to quit
 cd ../scripts
